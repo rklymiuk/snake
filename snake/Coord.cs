10d62cd86545bf017d40ea9bc6ba4f8ae51198e0
rklymiuk <klymrom05@gmail.com>
2024-11-17 19:00:43 +0200

    dfkkd

---
+++ snake/Coord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    internal class Coord
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Coord other = (Coord)obj;
+            return x == other.x && y == other.y;
+
+            
+        }
+    }
+}
---
+++ snake/Program.cs
@@ -0,0 +1,39 @@
+﻿using snake;
+Random rand = new Random();
+
+Coord gridDimensions = new Coord(50, 20);
+int SnakeX = rand.Next(1, gridDimensions.X-1);
+int SnakeY = rand.Next(1, gridDimensions.Y-1);
+Coord applePos = new Coord(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
+Coord snakePos = new Coord(SnakeX,SnakeY);
+int frameDelayMill = 100;
+
+while (true)
+{
+    Console.Clear();
+    for (int y = 0; y < gridDimensions.Y; y++)
+    {
+        for (int x = 0; x < gridDimensions.X; x++)
+        {
+            Coord currentCoord = new Coord(x, y);
+            if (snakePos.Equals(currentCoord))
+            {
+                Console.Write("■");
+            }
+            else if (applePos.Equals(currentCoord))
+            {
+                Console.Write("@");
+            }
+            else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+    Thread.Sleep(frameDelayMill);
+}
